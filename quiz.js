4908d22a7cb5e09353e8fcfb2eee8cbfa1900247
Ha-Mundo <trauzzu@gmail.com>
2020-09-13 09:09:39 +0200

    few changes

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', e=>{
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value,form.q4.value]; 
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]; 
 
   //CHECK ANSWERS
   userAnswers.forEach((answer, index) =>{
