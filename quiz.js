65791e0a7475be68fd6082c1884313c326da16f7
Ha-Mundo <trauzzu@gmail.com>
2021-10-07 09:27:06 +0200

    fix JS comments

--- quiz.js
+++ quiz.js
@@ -1,42 +1,38 @@
-const correctAnswers = ['A','B','B','C'];
-const form = document.querySelector('.quiz-form');
-const result = document.querySelector('div.d-none');
+const correctAnswers = ["A", "B", "B", "C"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector("div.d-none");
 
-form.addEventListener('submit', e=>{
+form.addEventListener("submit", e => {
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]; 
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
 
-  //CHECK ANSWERS
-  userAnswers.forEach((answer, index) =>{
-   
-   if(answer === correctAnswers[index]){
-     score += 25;
-     
-   }
+  // CHECK ANSWERS
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
   });
- 
 
+  // Show result on page
 
-  //Show result on page 
+  scrollTo(0, 0); // When we submit got to x and y = 0 (top)
+  result.querySelector("span").textContent = `${score}% `;
+  result.classList.remove("d-none");
 
-  scrollTo(0,0); // When we submit got to x and y = 0 (top)
-  result.querySelector('span').textContent = `${score}% `;
-  result.classList.remove('d-none');
-
-  let output = 0 ;
+  let output = 0;
   const timer = setInterval(() => {
-    
-    result.querySelector('span').textContent = `${output}% `;
-    if(output === score){
+    result.querySelector("span").textContent = `${output}% `;
+    if (output === score) {
       clearInterval(timer);
-     } else{
-        output++;
-      }
-   
-
+    } else {
+      output++;
+    }
   }, 10);
-
 });
-
